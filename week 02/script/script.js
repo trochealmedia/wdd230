2f51e39dda51cc0b1b1f5c8fd4c9786492bd9d95
trochealmedia <jonatancolbol@gmail.com>
2022-05-05 04:03:34 -0400

    week 02

---
+++ week 02/script/script.js
@@ -0,0 +1,6 @@
+let d = new Date();
+let fulldate = d.getFullYear();
+document.getElementById("currentdate").textContent = fulldate;
+
+const Update = document.lastModified
+document.querySelector("#update").textContent = Update;
