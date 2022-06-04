16b69bceb5494636cc6c2d6d92124145f1c312dd
trochealmedia <jonatancolbol@gmail.com>
2022-06-04 00:02:19 -0400

    discover page

---
+++ chamber_07/script/script.js
@@ -0,0 +1,7 @@
+// -------------------hamburguer-----------------------------
+function toggleMenu() {
+	document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburguer")
+x.onclick = toggleMenu;
